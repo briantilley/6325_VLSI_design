6db402dad685fc38c638a32dd40dc5b666fb56d4
Edward Hong <edward.xc.hong@gmail.com>
2018-09-04 20:48:43 -0500

    fix decoder4.v and tb

--- decoder4/decoder4.v
+++ decoder4/decoder4.v
@@ -1,25 +1,31 @@
-`include "submodules/equality_comparator/comparator.v"
+// `include "comparator.v"
+// `include "SIPO.v"
 `timescale 1ns/1ps
 
 module decoder4(
     output out, 
-    input clk, 
+    input clk,
+    input clr, 
     input sig, 
     input prgm);
 
-	// input types
-	wire clk, sig, prgm;
+    // input types
+    // wire clk, sig, prgm;
 	
-	// binary output for pattern/signal detection
-	wire out;
+    // binary output for pattern/signal detection
+    // wire out;
 
     // inputs to comparator
-    reg [3:0] a, b;
+    wire [3:0] a, b;
 
     // instantiate submodules
-    comp4 comparator(.eq(out), .a(a), .b(a));
+    // PROGRAM register
+    SIPO prgm_register(.clk(clk), .clear(clr), .in(prgm), .out(a));
 
-    // TODO: Instantiate PROGRAM and SIGNAL shift registers
-    // TODO: Connect comparator inputs to shift register outputs
+    // SIGNAL register
+    SIPO sig_prgm_register(.clk(clk), .clear(clr), .in(sig), .out(b));
+
+    // comparator
+    comp4 comparator(.eq(out), .a(a), .b(a));
 
 endmodule
--- decoder4/decoder4_tb.v
+++ decoder4/decoder4_tb.v
@@ -3,27 +3,31 @@
 module decoder4_tb;
   
 	// decoder inputs
-	wire clk, sig, prgm;
+	reg clk, clr, sig, prgm;
 
 	// decoder output
 	wire out;
 
-	// instantiate
-	decoder4 uut (.out(out), .clk(clk), .sig(sig), .prgm(prgm));
+    // program and signal sequences
+    reg[3:0] pattern = 4'b1010;
+    reg[15:0] seq = 16'b1010000101000000;
 
-	// initialize
+    // "counter" to ensure we load the entire programmed pattern and do not overwrite it
+    reg[3:0] stop = 4'b1111;
+
+	// instantiate unit under test
+	// decoder4 uut (.out(out), .clk(clk), .clr(clr), .sig(sig), .prgm(prgm));
+
 	initial begin
-		clk = 1; 
-        sig = 0;
-        prgm = 0;
+        clk = 1; 
+        clr = 1;
 
-        // Set PROGRAM and SIGNAL inputs
-        #10 
-        sig = 1;
-        prgm = 1;
+        // Wait for clear
+        #10
+        clr = 0;
 
         // Wait for registers to be loaded
-		#100;
+        #20;
 	end
 
     // Clock generator
@@ -31,6 +35,23 @@ module decoder4_tb;
        // Toggles clock every 5 ticks
        #5  clk = ~clk;
     end
+
+    // Load Sequence
+    always @(negedge clk) begin
+        // Load the bottom bit of the sequence
+        if(stop[3] !== 0) begin
+            prgm = pattern[3];
+            pattern = pattern << 1;
+            stop = stop << 1;
+	    end
+        sig = seq[15];
+
+        // Shift to the next bit in the sequence
+        seq = seq << 1;
+    end
+
+    decoder4 uut(.out(out), .clk(clk), .clr(clr), .sig(sig), .prgm(prgm));
+
 
 	// Test
 	//initial begin
