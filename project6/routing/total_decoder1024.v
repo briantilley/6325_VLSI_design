c6a0685e32e4204ffc88bb43abe8c7c429b5e589
Brian James Tilley <bjt150130@engnx10.utdallas.edu>
2018-12-05 07:44:41 -0600

    changes to make decoder top-level module

--- project6/routing/total_decoder1024.v
+++ project6/routing/total_decoder1024.v
@@ -1,3 +1,35 @@
+`timescale 1ns/1ps
+
+module decoder1024(
+    output out, 
+    input clk,
+    input clr,
+    input enable, 
+    input sig, 
+    input prgm);
+
+    // input types
+    // wire clk, sig, prgm;
+	
+    // binary output for pattern/signal detection
+    // wire out;
+
+    // inputs to comparator
+    wire [1023:0] a, b;
+
+    // instantiate submodules
+    
+    // PROGRAM register
+    SIPO1024 prgm_register(.clk(clk), .clear(clr), .in(prgm), .out(a), .enable(enable));
+
+    // SIGNAL register
+    SIPO1024 sig_prgm_register(.clk(clk), .clear(clr), .in(sig), .out(b), .enable(1'b1));
+
+    // comparator
+    comp1024 comparator(.eq(out), .a(a), .b(b));
+
+endmodule
+
 module SIPO1024(clk, clear, enable, in, out);
 
 	parameter SIZE = 1024;
@@ -40,36 +72,4 @@ module comp1024(eq, a, b);
   assign eq = ~| (a ^ b);
 
 endmodule
-// `include "comparator.v"
-// `include "SIPO.v"
-`timescale 1ns/1ps
 
-module decoder1024(
-    output out, 
-    input clk,
-    input clr,
-    input enable, 
-    input sig, 
-    input prgm);
-
-    // input types
-    // wire clk, sig, prgm;
-	
-    // binary output for pattern/signal detection
-    // wire out;
-
-    // inputs to comparator
-    wire [1023:0] a, b;
-
-    // instantiate submodules
-    
-    // PROGRAM register
-    SIPO1024 prgm_register(.clk(clk), .clear(clr), .in(prgm), .out(a), .enable(enable));
-
-    // SIGNAL register
-    SIPO1024 sig_prgm_register(.clk(clk), .clear(clr), .in(sig), .out(b), .enable(1'b1));
-
-    // comparator
-    comp1024 comparator(.eq(out), .a(a), .b(b));
-
-endmodule
